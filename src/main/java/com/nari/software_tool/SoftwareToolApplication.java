81657675987f73a9ff9e12d192e5f7cacc5bcc80
yinyx <302699832@qq.com>
2020-03-13 13:53:23 +0800

    项目上传

---
+++ src/main/java/com/nari/software_tool/SoftwareToolApplication.java
@@ -0,0 +1,14 @@
+package com.nari.software_tool;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nari/software_tool/SoftwareToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nari.software_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

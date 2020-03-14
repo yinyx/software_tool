edeccccf353dfb043b388f1d1ceba9d229fa69b9
zhangheng00069952 <z.heng.987654@163.com>
2020-03-14 11:52:56 +0800

    git push test

--- src/test/java/com/nari/software_tool/SoftwareToolApplicationTests.java
+++ src/test/java/com/nari/software_tool/SoftwareToolApplicationTests.java
@@ -2,7 +2,7 @@ package com.nari.software_tool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//git push test
 @SpringBootTest
 class SoftwareToolApplicationTests {
 

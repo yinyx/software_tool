b8f677b8aecb81b66d5ba8356f3a0fbcfe3b1c62
yinyx <302699832@qq.com>
2020-03-16 13:14:15 +0800

    系统Mapper

---
+++ src/main/java/com/nari/software_tool/dao/SoftPathInfoMapper.java
@@ -0,0 +1,15 @@
+package com.nari.software_tool.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author yinyx
+ * @version 1.0 2020/3/16
+ */
+@Mapper
+@Repository
+public interface SoftPathInfoMapper {
+
+
+}

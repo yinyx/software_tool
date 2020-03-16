4c4f9189905b883d1543d1eeffd4ce4c67b71b6d
yinyx <302699832@qq.com>
2020-03-16 14:36:28 +0800

    Controller

---
+++ src/main/java/com/nari/software_tool/controller/FileController.java
@@ -0,0 +1,14 @@
+package com.nari.software_tool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author yinyx
+ * @version 1.0 2020/3/16
+ */
+@Controller
+@RequestMapping
+public class FileController {
+
+}

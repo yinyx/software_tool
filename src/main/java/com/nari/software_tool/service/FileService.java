cb9d96d2ffa2ad05960930e554088edd4fbea0a1
yinyx <302699832@qq.com>
2020-03-16 12:09:16 +0800

    Service 结构

---
+++ src/main/java/com/nari/software_tool/service/FileService.java
@@ -0,0 +1,10 @@
+package com.nari.software_tool.service;
+
+/**
+ * @author yinyx
+ * @version 1.0 2020/3/14
+ */
+public interface FileService {
+
+
+}
---
+++ src/main/java/com/nari/software_tool/service/serviceImpl/FileServiceImpl.java
@@ -0,0 +1,17 @@
+package com.nari.software_tool.service.serviceImpl;
+
+import com.nari.software_tool.service.FileService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yinyx
+ * @version 1.0 2020/3/14
+ */
+@Service
+public class FileServiceImpl implements FileService {
+
+
+
+
+
+}

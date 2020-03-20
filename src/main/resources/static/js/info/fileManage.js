85312553e73d1d1019b3414c92f7bed79344f0b4
yinyx <302699832@qq.com>
2020-03-20 17:10:37 +0800

    界面修改

--- src/main/resources/static/js/info/fileManage.js
+++ src/main/resources/static/js/info/fileManage.js
@@ -17,9 +17,9 @@ function sendFile() {
         formData.append("soft",file_soft.files[0]);
         var name = $(":input[name='softwareName']").val();
         var nameEn = $(":input[name='softwareName_en']").val();
-        var introduction = $(":input[name='introduction']").val();
+        var introduction = $(":input[name='description']").val();
         var latestVersion = $(":input[name='version']").val();
-        var kind = $(":input[name='type']").val();
+        var kind = $(":radio[name=\"type\"]:checked").val();
         var data = {
             "name":name,
             "nameEn":nameEn,
@@ -29,10 +29,45 @@ function sendFile() {
         };
         formData.append("softwareForm",JSON.stringify(data));
 
-        request.open("POST","uploadSoftware");
+        request.open("POST","software/uploadSoftware");
         request.send(formData);
     };
 }
+
+function showKind(){
+    $.ajax({
+        url:"softwareKind/queryAllKinds",
+        type:"post",
+        dataType: "text",
+        contentType: "application/json",
+        success:function (data) {
+            data = $.parseJSON(decrypt(data,"abcd1234abcd1234"));
+            if(data.status == "success")
+            {
+                for (var i = 0; i < data.dataList.length; i++) {
+                    var html = "";
+                    console.log(data.dataList[i].id);
+                    html += '<label class="control-label">';
+                    html += '<input type="radio" name="type" id="type" value= "'+data.dataList[i].id+'" >' +data.dataList[i].kind_name;
+                    html += '</label>';
+                    $("#type_radio").append(html);
+                }
+            }else{
+                showSuccessOrErrorModal(data.msg,"error");
+            }
+        }
+    })
+
+}
+
 $(document).ready(function () {
+    //判断是否登录
+    userMap = isLogined();
+    if(userMap){//成功登录
+        userId = userMap.id;
+    }else{
+        //parent.location.href = jQuery.getBasePath() + "/login.html";
+    }
+    showKind();
     sendFile();
 });
